c554781b4caa6c574781a4de7abd00a534f80e2b
Cyanide <miqueas736d@gmail.com>
2026-03-05 12:35:22 -0300

    feat: adding nickname unit  testing

--- unit_testing/test_client.py
+++ unit_testing/test_client.py
@@ -1,11 +1,13 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from validations import validate_message
+from validations import validate_message, validate_nickname
 import unittest
 
 class TestWriteFunction(unittest.TestCase):
     
+    
+    # CLIENT INPUT VALIDATION TESTS--------
     def test_empty_message(self):
         self.assertFalse(validate_message(""),"empty messages are not allowed")
         
@@ -20,6 +22,23 @@ class TestWriteFunction(unittest.TestCase):
         
     def test_valid_message_with_sapces(self):
         self.assertTrue(validate_message("  hey  "))
+    
+    #CLIENT NICKNAME VALIDATION TESTS
+    
+    def test_empty_nickname(self):
+        self.assertFalse(validate_nickname(""),"empty messages are not allowed")
+        
+    def test_refuse_only_spaces_in_nick(self):
+        self.assertFalse(validate_nickname("    "),"spaces only are not allowed")
+        
+    def test_refuse_long_nicknames(self):
+        self.assertFalse(validate_nickname("a"*11),"message too long")
+        
+    def test_valid_message(self):
+        self.assertTrue(validate_nickname("miki"))
+        
+    def test_valid_message_with_sapces(self):
+        self.assertTrue(validate_nickname("  miki  "))
         
         
 if __name__ == "__main__":
