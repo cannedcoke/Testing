e6af9c4cbf58eb3273d7d38c792d084bfac328c5
Cyanide <miqueas736d@gmail.com>
2026-03-10 15:39:11 -0300

    fix: fixing validations

--- validations.py
+++ validations.py
@@ -1,16 +1,33 @@
+# GREEN: se implementó validate_message de forma mínima para que pasen los tests
+#       def validate_message(msg):
+#           if not msg.strip():
+#               return False
+#           return True
 def validate_message(msg):
     msg = msg.strip()
     
     if not msg:
         return False
-
+# GREEN: se agregó la validación de longitud:
+#       if len(msg) > 50:
+#           return False
     if len(msg) > 50:
         return False
     return True
 
+# REFACTOR: se limpió el código haciendo el strip una sola vez al principio
+#       def validate_message(msg):
+#           msg = msg.strip()
+#           if not msg:
+#               return False
+#           if len(msg) > 50:
+#               return False
+#           return True
+
 def formatted_message(nick,msg):
     return f"{nick}: {msg.strip()}"
 
+# function taht validates if the name is valid
 def validate_nickname(nick):
     nick = nick.strip()
     
@@ -20,14 +37,15 @@ def validate_nickname(nick):
     if len(nick) > 10:
         return False
     return True
-    
+
+# enters the message
 def clientMsg(nick):
     msg = input(">")
     if not validate_message(msg):
         return False
     return formatted_message(nick,msg)
         
-    
+# obtanines the nickname
 def get_nickname():
     while True:
         nick = input("Enter a nickname: ")
